554aa7d23664d9eea9cfe82ad4fcbc07eac1b3f6
unknown <abhishekrdhanke636@gmail.com>
2022-01-11 19:37:04 +0530

    forgot to remove frontend

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(require("./routes/post"))
 app.use(require("./routes/user"))
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('frontend/client/build'))
+    app.use(express.static('client/build'))
     const path = require("path");
     app.get("*",(req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
