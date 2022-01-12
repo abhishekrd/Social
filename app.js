54df6fcede02d2973bc3785a6b0416ee0206dd34
unknown <abhishekrdhanke636@gmail.com>
2022-01-12 13:49:33 +0530

    update

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ if(process.env.NODE_ENV=="production"){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
-app.listen(PORT,() => {
+app.listen(PORT,() => {  
     console.log(`Server started running at port ${PORT}`)
 })
