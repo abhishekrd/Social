ac521a7ae620a459b314788af7693800a3672920
unknown <abhishekrdhanke636@gmail.com>
2022-01-12 10:55:19 +0530

    Added Footer

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='foot-div'>
+            <h5 className='dev_id'>Coded with love by Abhishek | </h5>
+            <p>
+            <Link to={{ pathname: "https://www.linkedin.com/in/abhishek-dhanke-226354216/" }} target="_blank" /><i class='bx bxl-linkedin-square' style='color:#008ffc'  ></i>
+            </p>
+        </div>
+    )
+}
+
+export default Footer
