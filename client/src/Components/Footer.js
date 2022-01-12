3e3be723dc36dc1803d626ceb7ffd029c6d79caa
unknown <abhishekrdhanke636@gmail.com>
2022-01-12 11:12:09 +0530

    added footer in routes

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='foot-div'>
-            <h5 className='dev_id'>Coded with love by Abhishek | </h5>
+            <h5 className='dev_id'>Coded with <i class='bx bxs-heart' style='color:#f60505'  ></i> by Abhishek | </h5>
             <p>
             <Link to={{ pathname: "https://www.linkedin.com/in/abhishek-dhanke-226354216/" }} target="_blank" /><i class='bx bxl-linkedin-square' style='color:#008ffc'  ></i>
             </p>
